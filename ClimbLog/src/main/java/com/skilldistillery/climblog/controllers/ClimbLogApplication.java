3a68b0586a07c0ded19d699e5d1d23e20973393f
allez_ryan_venga <120065584+MiAmigoRyan@users.noreply.github.com>
2023-05-12 17:17:02 -0600

    finished config of boot and built webapp

---
+++ ClimbLog/src/main/java/com/skilldistillery/climblog/controllers/ClimbLogApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.climblog.controllers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClimbLogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClimbLogApplication.class, args);
+	}
+
+}
---
+++ ClimbLog/src/main/java/com/skilldistillery/climblog/data/ClimbLogApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.climblog.data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClimbLogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClimbLogApplication.class, args);
+	}
+
+}
